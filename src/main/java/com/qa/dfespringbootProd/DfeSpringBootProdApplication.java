d3b22304419159801e5b0cf3ab39ac2536c68408
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-11 19:34:55 +0100

    AE : My first commit

---
+++ src/main/java/com/qa/dfespringbootProd/DfeSpringBootProdApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringbootProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeSpringBootProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeSpringBootProdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfespringbootProd/DfeSpringBootProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringbootProd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeSpringBootProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
