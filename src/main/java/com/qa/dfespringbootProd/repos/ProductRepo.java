87777b1516d32e12d31a5fd3e58e1376995b21f6
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 17:21:46 +0100

    AE: Importing the util list Array

--- src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
+++ src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
@@ -1,12 +1,14 @@
 package com.qa.dfespringbootProd.repos;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ProductRepo extends JpaRepository {
 	
+	List<Product> findProductByProductName(String productName);    // to implement further to implement the stretch goal by searching for product by name. This is called the custom search	
 	
-
 
 }
