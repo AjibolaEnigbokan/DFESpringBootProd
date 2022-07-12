d6377e47b9710071345dfea27b882c391aab9a41
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 16:33:34 +0100

    AE: Added PUT and DELET Requests and the FindByProductName custome search

--- src/main/java/com/qa/dfespringbootProd/services/ProductService.java
+++ src/main/java/com/qa/dfespringbootProd/services/ProductService.java
@@ -7,12 +7,65 @@ public class ProductService {
 	
 	// here I will be using database instead of ArrayList for persistence
 	
-	private ProductRepo repo; // In order to link ProductService class with Repo, which must create a ProductREpo class and make it as variable for Productservice class
+	private ProductRepo repo; // In order to link ProductService class with Repo, which must create a ProductREpo class and make it as variable for ProductService class
 	
-	public ProductService(ProductRepo repo) {  // I need to also create a constructor of ProductService class and make ProductRepo and an input in the contsructor
-		
-		
+	public ProductService(ProductRepo repo) {  // I need to also create a constructor of ProductService class and make ProductRepo and an input in the constructor
+			
 	}
 	
+	// (GET - READ)
+		// ReadAll
 
+		public List<Product> readAll() {
+
+			return this.repo.findAll();
+		}
+
+		// ReadByID will be used in DataBase but cos we are using List Array we will use
+		// index.
+		
+		
+		public Product readById(long id) {
+
+			return this.repo.findById(id).get();
+		}
+
+		public List<Product> readByProductName(String productName) { // ReadByProductName this is a custom search to
+																		// implement the stretch goal
+			return this.repo.findProductByProductName(productName);
+		}
+
+		// Post -CREATE
+
+		public Product create(Product product) {
+			return this.repo.saveAndFlush(product);
+		}
+		
+		// PUT -UPDATE
+
+		public Product update(long id, Product product) {
+
+			// 1-we need to get existing entry
+			Product existingProduct = this.repo.findById(id).get();
+			// 2- Change the existing entry , using our new object above
+
+			existingProduct.setProductName(product.getProductName());
+			existingProduct.setProductPrice(product.getProductPrice());
+			existingProduct.setProductType(product.getProductType());
+			// save the product entry back into the database
+			return this.repo.saveAndFlush(existingProduct);
+
+		}
+
+		// DELETE - DELETE
+
+		public boolean delete(long id) {
+
+			this.repo.deleteById(id);
+			return !this.repo.existsById(id); // existsById sHould be false but must be flipped or invert to make it true.
+		}
+
+	
+	 
+	
 }
