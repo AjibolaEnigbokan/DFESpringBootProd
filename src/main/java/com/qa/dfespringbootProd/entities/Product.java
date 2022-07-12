94052f759f9662b7726206fd24347de097b5997a
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 09:47:25 +0100

    AE: second commit in my entity by importing generated values

--- src/main/java/com/qa/dfespringbootProd/entities/Product.java
+++ src/main/java/com/qa/dfespringbootProd/entities/Product.java
@@ -1,9 +1,19 @@
 package com.qa.dfespringbootProd.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Product {
 	
-
+	// define  Table columns
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private long id;
+	
+	
+	
+	
 }
