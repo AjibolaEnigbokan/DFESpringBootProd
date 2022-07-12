8da19219c90e179d5b03ce6bd068bbe745335db7
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 17:30:00 +0100

    AE:Corrected all errors in ProductRepo before merging

--- src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
+++ src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
@@ -1,6 +1,5 @@
 package com.qa.dfespringbootProd.repos;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,11 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import com.qa.dfespringbootProd.entities.Product;
 
-
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
-	
-	List<Product> findProductByProductName(String productName);    // to implement further to implement the stretch goal by searching for product by name. This is called the custom search	
-	
+
+	List<Product> findProductByProductName(String productName); // to implement further to implement the stretch goal by
+																// searching for product by name. This is called the
+																// custom search
 
 }
