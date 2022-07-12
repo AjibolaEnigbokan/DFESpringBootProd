3547fa5f31aa57882d7ae7ce5c34ee8f4702e86c
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 16:25:47 +0100

    AE: Creating a ProductService contructor and making ProductRepo as an input

--- src/main/java/com/qa/dfespringbootProd/services/ProductService.java
+++ src/main/java/com/qa/dfespringbootProd/services/ProductService.java
@@ -1,5 +1,18 @@
 package com.qa.dfespringbootProd.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+	
+	// here I will be using database instead of ArrayList for persistence
+	
+	private ProductRepo repo; // In order to link ProductService class with Repo, which must create a ProductREpo class and make it as variable for Productservice class
+	
+	public ProductService(ProductRepo repo) {  // I need to also create a constructor of ProductService class and make ProductRepo and an input in the contsructor
+		
+		
+	}
+	
 
 }
