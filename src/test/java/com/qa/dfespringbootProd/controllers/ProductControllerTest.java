be484204ca98009f6f097323f87f6ab53601eea3
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 20:27:33 +0100

    AE: Adding SpringBootTest ,AutoConfigureMockMvc annotations to the ProductControllerTest class for testing

--- src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
+++ src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
@@ -1,10 +1,18 @@
 package com.qa.dfespringbootProd.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql","classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class ProductControllerTest {
 	 
 	
 
 }
+ 
