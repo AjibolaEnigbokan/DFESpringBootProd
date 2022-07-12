7b98d8b61c97dfb96413a18f6201c560aaf82c4a
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 21:21:00 +0100

    AE: Added test script with input utility

--- src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
+++ src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
@@ -1,16 +1,56 @@
 package com.qa.dfespringbootProd.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Sql(scripts = {"classpath:testschema.sql","classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class ProductControllerTest {
+	
+	@Autowired
+	private MockMvc mvc; // used for sending mock request
+	
+	@Autowired
+	private ObjectMapper mapper;  //used for converting objects to JSON
+	
+	@Test
+	
+	public void readAllTest() throws Exception{
+		
+		List<Product> output = new ArrayList<>();
+		
+		Product entry = new Product(1L, "Apro" , 2.10, "Youghout");
+		output.add(entry);
+		
+		//Convert my expected output to JSON
+		
+		String outputAsJSON = mapper.writeValueAsString(output);
+		mvc.perform(get("/student/readAll")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(outputAsJSON));
+		
+		
+		
+	}
+	
+	
 	 
 	
 
