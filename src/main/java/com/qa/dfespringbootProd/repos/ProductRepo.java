636b00eff5da92a05b534ec8a7b038d28552c1f6
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-12 17:25:03 +0100

    AE:Importing the Product utility to implement custom search commit in ProductRepo

--- src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
+++ src/main/java/com/qa/dfespringbootProd/repos/ProductRepo.java
@@ -1,12 +1,16 @@
 package com.qa.dfespringbootProd.repos;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.qa.dfespringbootProd.entities.Product;
+
 
 @Repository
-public interface ProductRepo extends JpaRepository {
+public interface ProductRepo extends JpaRepository<Product, Long> {
 	
 	List<Product> findProductByProductName(String productName);    // to implement further to implement the stretch goal by searching for product by name. This is called the custom search	
 	
