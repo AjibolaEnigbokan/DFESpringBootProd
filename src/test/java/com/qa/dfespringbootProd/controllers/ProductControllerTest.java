61ac0b27d115ba92f90a8b129587a2c02d9fa059
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-14 00:40:05 +0100

    AE:commit before the merging

--- src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
+++ src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
@@ -43,8 +43,7 @@ public class ProductControllerTest {
 		// Convert my expected output to JSON
 
 		String outputAsJSON = mapper.writeValueAsString(output);
-		mvc.perform(get("/product/readAll")
-				.contentType(MediaType.APPLICATION_JSON))
+		mvc.perform(get("/product/readAll").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(content().json(outputAsJSON));
 
 	}
@@ -57,9 +56,7 @@ public class ProductControllerTest {
 		Product result = new Product(2L, "Bombay Bag", 30.0, "HandBag", "Black");
 		String resultAsJSON = mapper.writeValueAsString(result);
 
-		mvc.perform(post("/product/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(entryAsJSON))
+		mvc.perform(post("/product/create").contentType(MediaType.APPLICATION_JSON).content(entryAsJSON))
 				.andExpect(content().json(resultAsJSON));
 
 	}
@@ -69,32 +66,27 @@ public class ProductControllerTest {
 		Product entry = new Product(1L, "Apro", 40.0, "Yoghurt", "Yellow");
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
 
-		mvc.perform(get("/product/readById/1")
-				.contentType(MediaType.APPLICATION_JSON))
+		mvc.perform(get("/product/readById/1").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(content().json(entryAsJSON));
 	}
 
 	@Test
-	public void updateTest() throws Exception { // running the updated test method on the Product controller class 
+	public void updateTest() throws Exception { // running the updated test method on the Product controller class
 		Product entry = new Product("Bombay Bag", 30.0, "HandBag", "Black");
-		Product result = new Product(1L, "Bombay Bag", 30.0, "HandBag", "Yellow");// by replacing the colour of the product with yellow
+		Product result = new Product(1L, "Bombay Bag", 30.0, "HandBag", "Yellow");// by replacing the colour of the
+																					// product with yellow
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
 		String resultAsJSON = this.mapper.writeValueAsString(result);
 
-		mvc.perform(put("/product/update/1")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(entryAsJSON))
+		mvc.perform(put("/product/update/1").contentType(MediaType.APPLICATION_JSON).content(entryAsJSON))
 				.andExpect(content().json(resultAsJSON));
-	
+
 	}
-	
-	
+
 	@Test
-	public void deleteTest() throws Exception {
-		mvc.perform(delete("/product/delete/1")
-				.contentType(MediaType.APPLICATION_JSON))
+	public void deleteTest() throws Exception { // running the deleteTest method
+		mvc.perform(delete("/product/delete/1").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(content().string("true"));
 	}
-	
 
 }
