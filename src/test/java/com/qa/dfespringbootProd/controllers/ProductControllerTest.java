2d353352ba66836d14350678381f91d95ccc6ded
BolaEnigbokan <jibolaenny@yahoo.com>
2022-07-13 18:21:00 +0100

    AE:Added the MockMvc util

--- src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
+++ src/test/java/com/qa/dfespringbootProd/controllers/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.qa.dfespringbootProd.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.ArrayList;
